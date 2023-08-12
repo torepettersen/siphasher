8b05a2c9cf43e0b14227cb9c00fbc4fae0bc7f53
Tore Pettersen <toreskog@live.com>
2023-08-12 19:01:37 +0200

    Initial commit

---
+++ lib/siphasher.ex
@@ -0,0 +1,18 @@
+defmodule Siphasher do
+  @moduledoc """
+  Documentation for `Siphasher`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Siphasher.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/siphasher_test.exs
@@ -0,0 +1,8 @@
+defmodule SiphasherTest do
+  use ExUnit.Case
+  doctest Siphasher
+
+  test "greets the world" do
+    assert Siphasher.hello() == :world
+  end
+end
